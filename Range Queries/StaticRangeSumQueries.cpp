11da9224daf208d0f7c37d5b68ab278c6dc52cf4
cd-x <whowantsthisemailid@gmail.com>
2022-07-30 19:44:50 +0530

    solved brute force

--- Range Queries/StaticRangeSumQueries.cpp
+++ Range Queries/StaticRangeSumQueries.cpp
@@ -2,5 +2,16 @@
 using namespace std;
 
 int main(){
-	
+	int n,q; cin>>n>>q;
+	vector<int> v;
+	v.push_back(0);
+	while(n--){
+		int cur; cin>>cur;
+		cur += v.back();
+		v.push_back(cur);
+	}
+	while(q--){
+		int l,r; cin>>l>>r;
+		cout<<v[r] - v[l-1]<<endl;
+	}
 }
