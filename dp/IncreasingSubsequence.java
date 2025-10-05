480ce2122ad663b7778eee500f5b5aa37fd9013d
rajrishivishwakarma <whowantsthisemailid@gmail.com>
2025-10-06 00:53:24 +0530

    increasing subsequence

---
+++ dp/IncreasingSubsequence.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class IncreasingSubsequence {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(bufferedReader.readLine());
+        int[] arr = new int[n];
+        StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+        for(int i=0;i<n;i++){
+            arr[i] = Integer.parseInt(stringTokenizer.nextToken());
+        }
+        System.out.println(lis(arr));
+    }
+    private static int lis(int[] arr){
+        List<Integer> list = new ArrayList<>();
+        for(int x : arr){
+            int index = Collections.binarySearch(list, x);
+            if(index < 0) index = -(index + 1);
+            if(index >= list.size()) list.add(x);
+            else list.set(index, x);
+        }
+        return list.size();
+    }
+}
