3ea9778bbb520237142f5f54a413f818064f9355
rajrishivishwakarma <whowantsthisemailid@gmail.com>
2026-01-07 03:22:17 +0530

    digit dp non cses

--- dp/digitdp/BennyAndBrokenOdometer.java
+++ dp/digitdp/BennyAndBrokenOdometer.java
@@ -1,3 +1,5 @@
+package digitdp;
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.Arrays;
--- dp/digitdp/Code1036.java
+++ dp/digitdp/Code1036.java
@@ -1,3 +1,5 @@
+package digitdp;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
